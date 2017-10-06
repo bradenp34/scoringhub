d11a2c8c1d545d6374e16bc37754ae3964848efe
Rich Argentieri <arge@Ned.local>
2017-10-06 12:31:56 -0500

    New data models

--- scoringhub/settings/base.py
+++ scoringhub/settings/base.py
@@ -55,8 +55,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'scoringhub',
-        'USER': 'scoringhub',
-        'PASSWORD': 'UPDATE!',
+        'USER': 'arge',
+        'PASSWORD': '',
         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
     }
