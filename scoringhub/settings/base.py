8a50188d85e14ad1675616fa39ae829eac95f229
braden <braden@localhost.localdomain>
2017-10-05 20:56:53 -0500

    password

--- scoringhub/settings/base.py
+++ scoringhub/settings/base.py
@@ -56,7 +56,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'scoringhub',
         'USER': 'scoringhub',
-        'PASSWORD': 'Thnsv$11',
+        'PASSWORD': 'UPDATE!',
         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
     }
